b3e3b1eab3a16adf03f8f60c0cd672cfd9179667
Zijian Zhou <zephyroszhou@gmail.com>
2025-10-14 10:26:12 +0800

    add deepdive

--- Mem1/gen_data/scripts/data_process.sh
+++ Mem1/gen_data/scripts/data_process.sh
@@ -1,10 +1,11 @@
 WORK_DIR=.
-LOCAL_DIR=$WORK_DIR/data/nq_hotpotqa_train
+LOCAL_DIR=$WORK_DIR/data/deepdive
 
 ## process multiple dataset search format train file
-DATA=nq,hotpotqa
-python $WORK_DIR/scripts/data_process/qa_search_train_merge.py --local_dir $LOCAL_DIR --data_sources $DATA
+# DATA=nq,hotpotqa
+# python $WORK_DIR/gen_data/data_process/qa_search_train_merge.py --local_dir $LOCAL_DIR --data_sources $DATA
+python $WORK_DIR/gen_data/data_process/deepdive.py --local_dir $LOCAL_DIR
 
 ## process multiple dataset search format test file
-DATA=nq,triviaqa,popqa,hotpotqa,2wikimultihopqa,musique,bamboogle
-python $WORK_DIR/scripts/data_process/qa_search_test_merge.py --local_dir $LOCAL_DIR --data_sources $DATA
+# DATA=nq,triviaqa,popqa,hotpotqa,2wikimultihopqa,musique,bamboogle
+# python $WORK_DIR/gen_data/data_process/qa_search_test_merge.py --local_dir $LOCAL_DIR --data_sources $DATA
--- Mem1/train/train_ppo.sh
+++ Mem1/train/train_ppo.sh
@@ -1,13 +1,17 @@
-export CUDA_VISIBLE_DEVICES=4,5,6,7
+export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 
 WAND_PROJECT='MEM1'
 
 # nq_hotpotqa
-export DATA_DIR='data/nq_hotpotqa_train_multi_2'
-export BASE_MODEL="Qwen/Qwen2.5-7B"
-export EXPERIMENT_NAME=TEST-QWEN2.5-7B-RELEASE
+export DATA_DIR='/home/project/11714281/zijian/tmp/MEM1/Mem1/data/deepdive'
+# export BASE_MODEL="Qwen/Qwen2.5-7B"
+export BASE_MODEL="Mem-Lab/DeepDive-SFT"
+export EXPERIMENT_NAME=DeepDive-MEM1-SFT-MAXTURNS-10
 export PROGRAM_ENTRY=verl.trainer.main_ppo
-export MAX_TURNS=6
+export MAX_TURNS=10
+
+ACTOR_MODEL=$BASE_MODEL
+CRITIC_MODEL=$BASE_MODEL
 
 # webshops
 # export DATA_DIR='data/webshop'
@@ -38,21 +42,21 @@ PYTHONUNBUFFERED=1 python3 -m $PROGRAM_ENTRY \
     data.max_obs_length=1000 \
     data.shuffle_train_dataloader=True \
     algorithm.adv_estimator=gae \
-    actor_rollout_ref.model.path=$BASE_MODEL \
+    actor_rollout_ref.model.path=$ACTOR_MODEL \
     actor_rollout_ref.actor.optim.lr=1e-6 \
     actor_rollout_ref.model.enable_gradient_checkpointing=true \
     actor_rollout_ref.model.use_remove_padding=False \
     actor_rollout_ref.actor.optim.lr_warmup_steps_ratio=0.05 \
     actor_rollout_ref.actor.ppo_mini_batch_size=64 \
-    actor_rollout_ref.actor.ppo_micro_batch_size=4 \
+    actor_rollout_ref.actor.ppo_micro_batch_size=8 \
     actor_rollout_ref.actor.fsdp_config.param_offload=true \
     actor_rollout_ref.actor.fsdp_config.grad_offload=true \
     actor_rollout_ref.actor.fsdp_config.optimizer_offload=true \
-    actor_rollout_ref.rollout.log_prob_micro_batch_size=4 \
-    actor_rollout_ref.rollout.tensor_model_parallel_size=1 \
+    actor_rollout_ref.rollout.log_prob_micro_batch_size=8 \
+    actor_rollout_ref.rollout.tensor_model_parallel_size=2 \
     actor_rollout_ref.rollout.name=vllm \
-    actor_rollout_ref.rollout.gpu_memory_utilization=0.8 \
-    actor_rollout_ref.ref.log_prob_micro_batch_size=4 \
+    actor_rollout_ref.rollout.gpu_memory_utilization=0.5 \
+    actor_rollout_ref.ref.log_prob_micro_batch_size=8 \
     actor_rollout_ref.ref.fsdp_config.param_offload=True \
     actor_rollout_ref.rollout.n_agent=1 \
     actor_rollout_ref.rollout.temperature=1 \
@@ -60,9 +64,9 @@ PYTHONUNBUFFERED=1 python3 -m $PROGRAM_ENTRY \
     critic.optim.lr=1e-5 \
     critic.model.use_remove_padding=False \
     critic.optim.lr_warmup_steps_ratio=0.015 \
-    critic.model.path=$BASE_MODEL \
+    critic.model.path=$CRITIC_MODEL \
     critic.model.enable_gradient_checkpointing=true \
-    critic.ppo_micro_batch_size=4 \
+    critic.ppo_micro_batch_size=8 \
     critic.model.fsdp_config.param_offload=true \
     critic.model.fsdp_config.grad_offload=true \
     critic.model.fsdp_config.optimizer_offload=true \
@@ -73,17 +77,16 @@ PYTHONUNBUFFERED=1 python3 -m $PROGRAM_ENTRY \
     +trainer.val_only=false \
     +trainer.val_before_train=false \
     trainer.default_hdfs_dir=null \
-    trainer.n_gpus_per_node=4 \
+    trainer.n_gpus_per_node=8 \
     trainer.nnodes=1 \
-    trainer.save_freq=20 \
+    trainer.save_freq=50 \
     trainer.test_freq=150 \
     trainer.project_name=$WAND_PROJECT \
-    trainer.experiment_name=$EXPERIMENT_NAME \
+    trainer.experiment_name=$EXPERIMENT_NAME-resume50 \
     trainer.total_epochs=3 \
     trainer.total_training_steps=500 \
     trainer.default_hdfs_dir=null \
-    trainer.default_local_dir=verl_checkpoints/$EXPERIMENT_NAME \
+    trainer.default_local_dir=verl_checkpoints/$EXPERIMENT_NAME-resume50 \
     max_turns=$MAX_TURNS \
     retriever.url="http://127.0.0.1:8013/retrieve" \
     retriever.topk=3 \
-    2>&1 | tee $EXPERIMENT_NAME.log
